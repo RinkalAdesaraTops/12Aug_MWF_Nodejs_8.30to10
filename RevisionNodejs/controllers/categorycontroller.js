73f712cb697a78190080fb9f07bf3bc8e77703da
RinkalAdesaraTops <167286606+RinkalAdesaraTops@users.noreply.github.com>
2024-09-23 09:06:48 +0530

    update

--- RevisionNodejs/controllers/categorycontroller.js
+++ RevisionNodejs/controllers/categorycontroller.js
@@ -1,21 +1,55 @@
 const CatModel = require('../models/categorymodel')
-const catfrm = (req,res)=>{
-    res.render('category')
+const catfrm = async(req,res)=>{
+    let catlist = await CatModel.find() 
+
+    res.render('category',{
+        'catlist':catlist,
+        'editcat':''
+    })
 }
 const savecat = async(req,res)=>{
     console.log(req.file);
     
-    let {name} = req.body
-    let result = new CatModel({
-        name:name,
-        image:req.file.filename
-    })
-    await result.save()
+    let {catid,name} = req.body
+    let result=''
+    let res_data = await CatModel.findById(catid)
+    if(catid != ''){
+        //update
+        result = await CatModel.findByIdAndUpdate(catid,{
+            name:name,
+            image:req.file != undefined ? req.file.filename : res_data.image
+        })
+    } else {
+        //insert
+        result = new CatModel({
+            name:name,
+            image:req.file.filename
+        })
+        await result.save()
+    }
+   
     if(result){
         res.redirect('/category')
     }
 
 }
+const deletecat = async(req,res)=>{
+    let id = req.params.id
+    let result = await CatModel.findByIdAndDelete(id)
+    if(result){
+        res.redirect('/category')
+    }
+}
+const editcat = async(req,res)=>{
+    let id = req.params.id
+    let catlist = await CatModel.find() 
+    let result = await CatModel.findById(id)
+    if(result){
+        res.render('category',{
+            catlist:catlist,
+            editcat:result
+        })
+    }
+}
 
-
-module.exports = {catfrm,savecat}
+module.exports = {catfrm,savecat,deletecat,editcat}
--- RevisionNodejs/routes/categoryroutes.js
+++ RevisionNodejs/routes/categoryroutes.js
@@ -1,22 +1,25 @@
 var express = require('express')
 var router = express.Router()
-const {catfrm,savecat} = require('../controllers/categorycontroller')
+const {catfrm,savecat,deletecat,editcat} = require('../controllers/categorycontroller')
 const multer = require('multer')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null,'./public/uploads')
     },
     filename: function (req, file, cb) {
-        let addExtension = file.originalname.split('.')
+        let addExtension = file.originalname.split('.') //abc.jpg = ['abc','jpg']
         
       const uniqueSuffix = Date.now()+'.'+addExtension[1]
       cb(null, file.fieldname + '-' + uniqueSuffix)
     }
   })
   
-  const upload = multer({ storage: storage })
+const upload = multer({ storage: storage })
 
 router.get('/',catfrm)
 router.post('/savecat',upload.single('image'),savecat)
+router.get('/delcat/:id',deletecat)
+router.get('/editcat/:id',editcat)
+
 
 module.exports = router
